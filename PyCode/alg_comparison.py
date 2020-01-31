031ad76f310a5d50b5de3972ca6b122e72c096b0
higorsmonteiro <higormonteiros@gmail.com>
2020-01-31 11:50:41 -0300

    timing

--- PyCode/alg_comparison.py
+++ PyCode/alg_comparison.py
@@ -3,33 +3,55 @@
     runtime measurements for each fibration algorithm and we store
     on data files.
 '''
+#import sys
+import timeit
+#import numpy as np
+#import graph_tool.all as gt
+#from fibration_mains import *
+
+### From the "graph tool library" we create a random directed network ###
+#def deg_sampler():
+#    return (np.random.randint(1,10), np.random.randint(1,10))
+#
+#N = int(sys.argv[1])
+#mode = sys.argv[2]
+#
+#g = gt.random_graph(N, deg_sampler, model="erdos", self_loops=True, random=True)
+#regulation = g.new_edge_property('int')
+#for n in g.edges():
+#    regulation[n] = 0
+#g.edge_properties['regulation'] = regulation
+#print(g.get_edges().shape[0])
+#########################################################################
+
+mysetup = '''
 import sys
 import numpy as np
 import graph_tool.all as gt
-from fibration_mains import *
+from fibration_mains import FFPartitioning, MBColoring    '''
 
-### From the "graph tool library" we create a random directed network ###
+test_code = '''
 def deg_sampler():
     return (np.random.randint(1,10), np.random.randint(1,10))
-
-N = int(sys.argv[1])
-mode = sys.argv[2]
-
-g = gt.random_graph(N, deg_sampler, model="erdos", self_loops=True, random=True)
+    
+g = gt.random_graph(50, deg_sampler(), model="erdos", self_loops=True, random=True)
 regulation = g.new_edge_property('int')
 for n in g.edges():
     regulation[n] = 0
-g.edge_properties['regulation'] = regulation
-print(g.get_edges().shape[0])
-#########################################################################
-
-if mode=="-mbc":
-    # -> Minimal balanced coloring algorithm <- #
-    MBColoring(g)
-elif mode=="-fpp":
-    # -> Fast fibration partitioning algorithm <- #
-    FFPartitioning(g)
-else:
-    print("Wrong mode flag.")
+g.edge_properties['regulation'] = regulation  
+FFPartitioning(g) '''
 
 
+
+
+#if mode=="-mbc":
+#    # -> Minimal balanced coloring algorithm <- #
+#    MBColoring(g)
+#elif mode=="-ffp":
+#    # -> Fast fibration partitioning algorithm <- #
+#    vt = timeit.timeit(FFPartitioning(g), number=50)
+#else:
+#    print("Wrong mode flag.")
+
+time = timeit.timeit(setup=mysetup, stmt=test_code, number=50)
+print(time)
